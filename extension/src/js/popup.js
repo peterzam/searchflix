efe739113cefe6f78f52dc8ce714e5938f69dd5f
Peter Zam <me@peterzam.dev>
2021-01-09 21:12:50 +0630

    fix

--- extension/src/js/popup.js
+++ extension/src/js/popup.js
@@ -5,23 +5,43 @@ function addEventListenersToResults(e) {
   });
 }
 
+function debounce(callback, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, delay);
+  };
+}
+
 $(document).ready(function () {
-  $('#searchCategory').keyup(function () {
-    var searchField = $('#searchCategory').val();
-    var searchExpression = new RegExp(searchField, 'i');
-    $.getJSON('data/categories.json', function (data) {
-      var resultOutput = '';
-      $.each(data, function (key, val) {
-        if (val.t.search(searchExpression) != -1) {
-          resultOutput += '<div class="result">';
-          resultOutput += '<a href="https://www.netflix.com/browse/genre/' + val.n + '">';
-          resultOutput += val.t;
-          resultOutput += '</a></div>';
-        }
+  $('#searchCategory').keyup(
+    debounce(function () {
+      var searchField = $('#searchCategory').val();
+      var resultCount = 0;
+      var searchExpression = new RegExp(searchField, 'i');
+      $.getJSON('data/categories.json', function (data) {
+        var resultOutput = '';
+        console.log(typeof searchExpression + searchExpression);
+        $.each(data, function (_, val) {
+          if (
+            searchExpression != '/(?:)/i' &&
+            val.t.search(searchExpression) != -1
+          ) {
+            resultOutput += '<div class="result"  class="tab-pane fade">';
+            resultOutput +=
+              '<a href="https://www.netflix.com/browse/genre/' + val.n + '">';
+            resultOutput += val.t;
+            resultOutput += '</a></div>';
+            console.log(searchField.length);
+            resultCount += 1;
+          }
+          if (resultCount > 500) {
+            return false;
+          }
+        });
+        $('#resultsList').html(resultOutput);
       });
-      $('#resultsList').html(resultOutput);
-    });
-  });
+    }, 100)
+  );
   $('body').on('click', '.result a', addEventListenersToResults);
 });
-
